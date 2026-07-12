8cf65b0dffbddf6733bd33e41bcdadf11883c764
Vaclav Petras <wenzeslaus@gmail.com>
2026-07-12 11:43:57 -0400

    libgis consumers: Use the etc binaries directory for executables
    The lock program in g.mapset and the seg and ram programs in the
    r.watershed command line construction were looked up with G_etc_dir(),
    which is the arch-independent data directory and differs from the
    binaries directory in the FHS layout. Use G_etcbin_dir() as the
    front-end of r.watershed and d.mon already do. This broke every
    gunittest creating a mapset on the FHS CI leg (lock file of mapset
    cannot be checked). Also look up the r.in.wms support modules through
    GRASS_ETCBINDIR instead of deriving the path from the script location.
    
    Verified on the FHS install: g.mapset -c switches mapsets and the
    previously failing test setUp proceeds past mapset creation.
    
    Local progress-tracking commit, not for a PR. Written with Claude
    Code.

--- general/g.mapset/main.c
+++ general/g.mapset/main.c
@@ -246,7 +246,7 @@ int main(int argc, char *argv[])
     if (!gis_lock)
         G_fatal_error(_("Unable to read GIS_LOCK environment variable"));
 
-    G_asprintf(&lock_prog, "%s/lock", G_etc_dir());
+    G_asprintf(&lock_prog, "%s/lock", G_etcbin_dir());
 
     snprintf(path, sizeof(path), "%s/.gislock", mapset_new_path);
     G_debug(2, "%s", path);
--- raster/r.watershed/shed/com_line.c
+++ raster/r.watershed/shed/com_line.c
@@ -54,7 +54,7 @@ int com_line_Gwater(INPUT *input, OUTPUT *output)
         input->com_line_ram = (char *)G_calloc(400, sizeof(char));
         prog_name = G_store(RAM_NAME);
         snprintf(input->com_line_ram, (400 * sizeof(char)), "\"%s/water/%s\"",
-                 G_etc_dir(), RAM_NAME);
+                 G_etcbin_dir(), RAM_NAME);
         fprintf(stderr,
                 "\nIf there is not enough ram for the fast mode (%s) to run,\n",
                 RAM_NAME);
@@ -64,7 +64,7 @@ int com_line_Gwater(INPUT *input, OUTPUT *output)
             input->slow = 1;
             input->com_line_seg = (char *)G_calloc(400, sizeof(char));
             snprintf(input->com_line_seg, (400, sizeof(char)),
-                     "\"%s/water/%s\"", G_etc_dir(), SEG_NAME);
+                     "\"%s/water/%s\"", G_etcbin_dir(), SEG_NAME);
         }
     }
     else {
@@ -72,7 +72,7 @@ int com_line_Gwater(INPUT *input, OUTPUT *output)
         prog_name = G_store(SEG_NAME);
         input->com_line_seg = (char *)G_calloc(400, sizeof(char));
         snprintf(input->com_line_seg, (400, sizeof(char)), "\"%s/water/%s\"",
-                 G_etc_dir(), SEG_NAME);
+                 G_etcbin_dir(), SEG_NAME);
     }
 
     G_message(_("\nIf you hit <return> by itself for the next question, this"));
